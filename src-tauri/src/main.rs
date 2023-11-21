93617dd19c9bc5783dad1b84c7cb0219229a1857
NalinPlad <43052612+NalinPlad@users.noreply.github.com>
2023-11-20 23:00:38 -0800

    setting up rust backend

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,10 +1,33 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+
+// Imports
+use std::process::Command;
+
+
 #[tauri::command]
 fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
+}
+
+#[tauri::command]
+
+async fn get_mac() -> String {
+    let defa
+
+    let ifc = String::from_utf8(
+        Command::new("ifconfig")
+            .arg("en0")
+            .output()
+            .expect("ifconfig failed")
+            .stdout
+    ).unwrap();
+
+    let 
+
+    return ifc;
+
 }
 
 fn main() {
